a30e9abad29db89b8b358a2726c363e9cc36d839
dpsingh11 <singh.dprakash@gmail.com>
2018-03-01 01:43:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter', fuction(req, res) {
+app.get('/counter', function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
