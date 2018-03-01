7332717e2f97f38e9348912105de72a5b3c94e7c
dpsingh11 <singh.dprakash@gmail.com>
2018-03-01 23:59:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,23 @@
-console.log('Loaded!');
-var img = document.getElementById('img');
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-};
-
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-
+//counter code
 var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
+button.onclick = function(){
+ //create a request object
+ var request = new XMLHttpRequest();
+ //capture the response and store it in a variable
+ request.onreadystatechange = function() {
+     if(request.readyState ===  XMLHttpRequest.DONE){
+         //take some action
+         if(request.status === 200) {
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+         }
+         }
+         //not done yet
+     };
+     //make the request
+     request.open('GET', 'http://deveshprakashsingh.imad.hasura.app.io/counter',true);
+     request.send(null);
+ };
+ 
 
