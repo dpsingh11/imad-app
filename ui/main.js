283d3868d2c96592e7ebb092f3f16a900dfebc49
dpsingh11 <singh.dprakash@gmail.com>
2018-03-03 00:03:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,13 +24,13 @@ button.onclick = function(){
 //submit name 
 var nameInput = document.getElementById('name');
 //var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
     var names = ['name1','name2','name3'];
     var list = '';
     for(var i=0; i<names.length;i++){
         list == '<li>' + names[i] + '<li>';
     }
-    var ul = document.getElementById('nameList');
+    var ul = document.getElementById("nameList");
     ul.innerHTML = list;
 };
