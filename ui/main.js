600fbb6e8c2ba5bebd73759384299d547a3b0eca
dpsingh11 <singh.dprakash@gmail.com>
2018-03-03 20:15:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick = function(){
          //not done yet
      };
      //make the request
-     request.open('GET', 'http://deveshprakashsingh.imad.hasura.app.io/counter',true);
+     request.open('GET', 'http://deveshprakashsingh.imad.hasura-app.io/counter',true);
      request.send(null);
  };
  
