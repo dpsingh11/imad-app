72100235b37791e2d80b91570e75a4cdf37aed16
Devesh Prakash Singh <singh.dprakash@gmail.com>
2018-03-09 18:04:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,9 +108,20 @@ function createTemplate(data){
 return htmlTemplate;
 }
 
-app.get('/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
+    //articleName = article-one
+    //articles[articleName]
      var articleName = req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
+     pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err,result) {
+         if(err){
+             res.status(500).send(err.toString());
+         }
+         else {
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+         }
+     });
+    
 });
 
 
