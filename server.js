d706d641102f590f0abd28ecea97ea09daf8d12e
Devesh Prakash Singh <singh.dprakash@gmail.com>
2018-03-09 14:03:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var config = {
+    user: 'deveshprakashsingh',
+    database:'deveshprakashsingh',
+    host: 'db.imad.hasura.app.io',
+    port:'5432',
+    password:process.emv.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -9,6 +18,20 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new pool(config);
+app.get('/test-db', function(req,res){
+   //make a select request
+   //retuen a response with the results
+   pool.query('SELECT * FROM test', function(err,result) {
+   if(err) {
+       res.status(500).send(err.toString());
+   }
+   else {
+       res.send(JSON.stringify(result));
+   }
+   });
+ });
 
 var counter = 0;
 app.get('/counter', function(req, res) {
