2b320cb533bf0015b1aa3788ec951e552636c6c7
Devesh Prakash Singh <singh.dprakash@gmail.com>
2018-03-18 17:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/hash/:input', function(req,res){
 function hash(input, salt){
     //how do we create a hash ?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString(hex);
+    return hashed.toString('hex');
 }
 
 var pool = new Pool(config);
