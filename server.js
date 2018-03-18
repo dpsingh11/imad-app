6a29d1ade10e9cab405e90a1c9ffcb5fd77f444b
Devesh Prakash Singh <singh.dprakash@gmail.com>
2018-03-18 17:47:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'deveshprakashsingh',
@@ -18,6 +19,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash(req.params.input,"this is some salt");
+    res.send(hashedString);
+});
+
+function hash(input, salt){
+    //how do we create a hash ?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed;
+}
 
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
